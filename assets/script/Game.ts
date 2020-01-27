08907e46fdfb2c83b15ac7633bafc4f6b5028bba
saxonliu <saxonliu@tencent.com>
2020-01-27 15:26:46 +0800

    增加星星随机出现逻辑

--- assets/script/Game.ts
+++ assets/script/Game.ts
@@ -13,15 +13,38 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class NewClass extends cc.Component {
 
-    @property(cc.Label)
-    star:cc.Prefab = null;
+    @property(cc.Prefab)
+    starPrefab:cc.Prefab = null;
 
+    @property(cc.Node)
+    player:cc.Node = null;
+
+    @property(cc.Node)
+    ground:cc.Node = null;
     @property
-    text: string = 'hello';
+    maxStarDuration: number = 0;
+    @property
+    minStarDuration: number = 0;
 
+    generateNewStar(){
+        let newStar=cc.instantiate(this.starPrefab)
+        this.node.addChild(newStar)
+        newStar.setPosition(this.generateRandomPosition())
+    }
+    
+    generateRandomPosition():cc.Vec2{
+        let maxX=this.node.width/2
+        let x=(Math.random()-0.5)*2*maxX+this.starPrefab.data.width/2
+        let baseY=this.ground.y+this.ground.height/2
+        let relativeY=this.player.getComponent("Player").jumpHeight*Math.random()+this.starPrefab.data.height/2
+        let y=baseY+relativeY
+        return cc.v2(x,y)
+    }
     // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {}
+    onLoad () {
+        this.generateNewStar()
+    }
 
     start () {
 
